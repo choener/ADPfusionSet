0c37a3a358d6c9a8fe00dc0e18291db346f38a64
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2017-01-24 22:23:35 +0100

    all Set2 goes into their own files
    more order

---
+++ ADP/Fusion/Set2.hs
@@ -0,0 +1,22 @@
+
+-- | Modules for @Set@-based dynamic programming. To be moved into
+-- @ADPfusionSet@
+
+module ADP.Fusion.Set2
+  ( module ADP.Fusion.Set2
+  , module ADP.Fusion.Core
+  , module ADP.Fusion.Core.Set2
+  , module ADP.Fusion.SynVar.Indices.Set2
+  , module ADP.Fusion.Term.Edge.Set2
+  , module ADP.Fusion.Term.Epsilon.Set2
+  , module ADP.Fusion.Term.Singleton.Set2
+  ) where
+
+import ADP.Fusion.Core
+
+import ADP.Fusion.Core.Set2
+import ADP.Fusion.SynVar.Indices.Set2
+import ADP.Fusion.Term.Edge.Set2
+import ADP.Fusion.Term.Epsilon.Set2
+import ADP.Fusion.Term.Singleton.Set2
+
--- ADP/Fusion/Term/Epsilon/Set2.hs
+++ ADP/Fusion/Term/Epsilon/Set2.hs
@@ -1,5 +1,5 @@
 
-module ADP.Fusion.Term.Epsilon.Set where
+module ADP.Fusion.Term.Epsilon.Set2 where
 
 import Data.Proxy
 import Data.Strict.Tuple
@@ -10,44 +10,44 @@ import ADP.Fusion.Core
 import Data.Bits.Ordered
 import Data.PrimitiveArray hiding (map)
 
-import ADP.Fusion.Core.Set
+import ADP.Fusion.Core.Set2
 
 
 
--- ** No boundaries
+-- ** Two boundaries
 
 instance
-  ( TmkCtx1 m ls Epsilon (BitSet i)
-  ) => MkStream m (ls :!: Epsilon) (BitSet i) where
+  ( TmkCtx1 m ls Epsilon (BS2 First Last i)
+  ) => MkStream m (ls :!: Epsilon) (BS2 First Last i) where
   mkStream (ls :!: Epsilon) sv us is
     = map (\(ss,ee,ii) -> ElmEpsilon ii ss)
     . addTermStream1 Epsilon sv us is
     $ mkStream ls (termStaticVar Epsilon sv is) us (termStreamIndex Epsilon sv is)
   {-# Inline mkStream #-}
 
-
-
 instance
-  ( TstCtx m ts s x0 i0 is (BitSet I)
-  ) => TermStream m (TermSymbol ts Epsilon) s (is:.BitSet I) where
-  termStream (ts:|Epsilon) (cs:.IStatic r) (us:.u) (is:.i)
+  ( TstCtx m ts s x0 i0 is (BS2 First Last I)
+  ) => TermStream m (TermSymbol ts Epsilon) s (is:.BS2 First Last I) where
+  termStream (ts:|Epsilon) (cs:.IStatic r) (us:.u) (is:.BS2 bs _ _)
     = map (\(TState s ii ee) ->
-              TState s (ii:.:RiBsI 0) (ee:.()) )
+              TState s (ii:.:RiBs2I (BS2 0 0 0)) (ee:.()) )
     . termStream ts cs us is
-    . staticCheck (i==0)
+    . staticCheck (bs==0)
   {-# Inline termStream #-}
 
 instance
-  ( TstCtx m ts s x0 i0 is (BitSet O)
-  ) => TermStream m (TermSymbol ts Epsilon) s (is:.BitSet O) where
-  termStream (ts:|Epsilon) (cs:.OStatic r) (us:.u) (is:.i)
+  ( TstCtx m ts s x0 i0 is (BS2 First Last O)
+  ) => TermStream m (TermSymbol ts Epsilon) s (is:.BS2 First Last O) where
+  termStream (ts:|Epsilon) (cs:.OStatic r) (us:.BS2 ub uf ul) (is:.BS2 bs f l)
     = map (\(TState s ii ee) ->
-              TState s (ii:.:RiBsO u u) (ee:.()) )
+              let io = getIndex (getIdx s) (Proxy :: PRI is (BS2 First Last O))
+              in  TState s (ii:.:io) (ee:.()) )
     . termStream ts cs us is
-    . staticCheck (i==u)
+    . staticCheck (ub==bs)
   {-# Inline termStream #-}
 
 
+{-
 
 instance TermStaticVar Epsilon (BitSet I) where
   termStaticVar _ sv _ = sv
@@ -60,4 +60,6 @@ instance TermStaticVar Epsilon (BitSet O) where
   termStreamIndex _ _ b = b
   {-# Inline [0] termStaticVar #-}
   {-# Inline [0] termStreamIndex #-}
+
+-}
 
