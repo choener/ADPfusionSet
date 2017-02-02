93544a896ef2da6eee820e4760b77a8f57131a7e
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2017-02-02 18:55:57 +0100

    updated Index generation

--- ADP/Fusion/SynVar/Indices/Set1.hs
+++ ADP/Fusion/SynVar/Indices/Set1.hs
@@ -41,10 +41,34 @@ instance
   -- Deal with @X -> Y e@ type rules.
   addIndexDenseGo (cs:.c) (vs:.IVariable rb) (lbs:._) (ubs:._) (us:.BS1 uSet uBnd) (is:.BS1 set bnd)
     = flatten mk step . addIndexDenseGo cs vs lbs ubs us is
-    where mk s = undefined
-          step = undefined
-          {-# Inline [0] mk   #-}
-          {-# Inline [0] step #-}
+          -- Extract how many bits are already set -- should be zero (!) --
+          -- and prepare the list of all possibilities.
+          -- In addition, at least one bit should be reserved, hence
+          -- @rb>0@. The boundary bit is one of those reserved and
+          -- constitutes a mask to be used further down.
+    where mk (SvS s t y') =
+            let RiBs1I (BS1 cset (Boundary to)) = getIndex (getIdx s) (Proxy :: PRI is (BS1 First I))
+                numBits = popCount cset
+                stopAt  = numBits - rb + 1
+                -- initialize to lowest set, that fullfills the
+                -- constraints. The @Boundary@ bit is not a valid bit to
+                -- set here!
+                initSet  = popShiftL initMask $ 2 ^ (popCount set - rb - numBits) - 1
+                initBnd  = Boundary $ lsbZ initSet
+            in  assert (numBits == 0 && to == (-1) && rb > 0)
+                $ return (SvS s t y', if rb > popCount set then Nothing else Just $ BS1 initSet initBnd)
+          step (_, Nothing)
+            = return Done
+          step (SvS s t y', Just (BS1 nset nbnd)) =
+            -- No need to provide @nset .|. getIndex s@ because no bit is
+            -- set in @getIndex s@.
+            return $ Yield (SvS s (t:.BS1 nset nbnd) (y':.:RiBs1I (BS1 nset nbnd)))
+                           ( SvS s t y', setSucc (BS1 0 (-1)) (BS1 initMask initBnd) (BS1 nset nbnd))
+          initMask = set `clearBit` getBoundary bnd
+          initBnd  = Boundary $ lsbZ initMask
+          {-# Inline [0] mk       #-}
+          {-# Inline [0] step     #-}
+          {-# Inline     initMask #-}
   {-# Inline addIndexDenseGo #-}
 
 {-
